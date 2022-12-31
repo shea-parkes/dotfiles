a72106263872927ecb43e190fdc0df11d367da19
Shea Parkes <shea.parkes@milliman.com>
2022-12-30 21:38:20 -0500

    Expand neovim setup.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ cd ~/.local
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 ./nvim.appimage --appimage-extract
-ln -s ~/.local/squashfs-root/AppRun nvim
+ln -s ~/.local/squashfs-root/AppRun ~/.local/bin/nvim
 
 # Bring in our custom neovim config
 echo "XDG_CONFIG_HOME=$HOME" >> .profile
@@ -13,3 +13,9 @@ git clone https://github.com/shea-parkes/neovim-config ~/.config/nvim
 cd ~/.config/nvim
 git submodule init
 git submodule update
+
+# Get Neovim mostly ready to go
+cd /workspaces/$RepositoryName
+poetry shell
+pip install pynvim ipython
+nvim --headless +":UpdateRemotePlugins" +"q!"
