934cfe867137bc63071759e4bb26d19843d2b277
Shea Parkes <shea.parkes@milliman.com>
2022-12-30 21:14:41 -0500

    Bring in neovim config.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-mkdir ~/bin
-curl -L https://github.com/neovim/neovim/releases/latest/download/nvim.appimage -o ~/.local/nvim.appimage
-chmod u+x ~/.local/nvim.appimage
-~/.local/nvim.appimage --appimage-extract
+# ~Manually install NeoVim
+cd ~/.local
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+./nvim.appimage --appimage-extract
+ln -s ~/.local/squashfs-root/AppRun nvim
+
+# Bring in our custom neovim config
+echo "XDG_CONFIG_HOME=$HOME" >> .profile
+mkdir ~/.config
+cd ~/.config
+git clone https://github.com/shea-parkes/neovim-config nvim
+cd ~/.config/nvim
+git submodule init
+git submodule update
