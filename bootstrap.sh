409c20be9010ce15ebe44110bd8cf8a88eb56c7d
Shea Parkes <shea.parkes@milliman.com>
2022-12-30 20:55:18 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 
+mkdir ~/bin
+curl -L https://github.com/neovim/neovim/releases/latest/download/nvim.appimage -o ~/.local/nvim.appimage
+chmod u+x ~/.local/nvim.appimage
+~/.local/nvim.appimage --appimage-extract
